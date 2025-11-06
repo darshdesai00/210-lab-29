a3715e24cc7856dc43be5a27eaa7c67ab123abc0
darshdesai00 <darshdesai50@icloud.com>
2025-11-05 16:53:03 -0800

    fixing some errors and working on pseudocode

--- main.cpp
+++ main.cpp
@@ -22,13 +22,12 @@ int main() {
 // Each key = company ticker (e.g., AAPL)
 // Each value = array with 3 lists for [price, volume, sentiment]
 map<string, array<list<double>, 3>> market;
-
+    
 // Open an external file to read initial stock data
 ifstream fin("stocks_data.txt");
 if (!fin) {
-cerr << "Error: could not open file." << endl;
-eturn 1;   
-}
+cerr << "Error: could not open stocks_data.txt" << endl;
+return 1;
 }
 
 string ticker;
@@ -36,4 +35,31 @@ double price, volume, sentiment;
 
 // Read data find file and populate the map
 // For each line get the company name and data
+// Add the price to the company’s price list
+// Add the volume to the company’s volume list
+// Add the sentiment to the company’s sentiment list
+while (fin >> ticker >> price >> volume >> sentiment) {
+market[ticker][0].push_back(price);
+market[ticker][1].push_back(volume);
+market[ticker][2].push_back(sentiment);
+}
+
+// Close the file
+fin.close();
+}
+
+// Display the initial state of the market
+// For each company:
+// Print ticker, latest price, volume, and sentiment
+cout << "Initial Market State:\n";
+for (auto &entry : market) {
+cout << entry.first
+ << " | Price: " << entry.second[0].back()
+ << " | Volume: " << entry.second[1].back()
+ << " | Sentiment: " << entry.second[2].back()
+ << endl;
+}
+
+// outputs start of simulation
+cout << "\n--- Begin Simulation ---\n" << endl;
 
