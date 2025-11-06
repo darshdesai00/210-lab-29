471602f53418cd0f50eb158588855773efae9a3d
darshdesai00 <darshdesai50@icloud.com>
2025-11-05 16:20:06 -0800

    started main.cpp with pseudocode and basic structure

---
+++ main.cpp
@@ -0,0 +1,23 @@
+// COMSC-210 | Lab 29 | Darsh Desai
+// IDE used: VS Code
+// Description: Stock Market Trends Simulation - (Pseudocode)
+
+// Include your headers: 
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <array>
+#include <list>
+#include <string>
+using namespace std;
+
+// Func prototype
+//void = no return value
+//simulate_day = your function name
+//(map<string, array<list<double>, 3>> &market, int day) = inputs
+void simulate_day(map<string, array<list<double>, 3>> &market, int day);
+
+int main() {
+    
+    
+}
