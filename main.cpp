a1873c39bc29d4296a281e34759a49484c7fca1b
darshdesai00 <darshdesai50@icloud.com>
2025-11-05 16:36:58 -0800

    working on pseudocode structure and file setup

--- main.cpp
+++ main.cpp
@@ -12,12 +12,28 @@
 using namespace std;
 
 // Func prototype
-//void = no return value
-//simulate_day = your function name
-//(map<string, array<list<double>, 3>> &market, int day) = inputs
+// void = no return value
+// simulate_day = your function name
+// (map<string, array<list<double>, 3>> &market, int day) = inputs
 void simulate_day(map<string, array<list<double>, 3>> &market, int day);
 
 int main() {
-    
-    
+// Initialize a map to store company information
+// Each key = company ticker (e.g., AAPL)
+// Each value = array with 3 lists for [price, volume, sentiment]
+map<string, array<list<double>, 3>> market;
+
+// Open an external file to read initial stock data
+ifstream fin("stocks_data.txt");
+if (!fin) {
+cerr << "Error: could not open file." << endl;
+eturn 1;   
 }
+}
+
+string ticker;
+double price, volume, sentiment;
+
+// Read data find file and populate the map
+// For each line get the company name and data
+
